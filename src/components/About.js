a4fc9e9bf4ce82e65eddf2d052d1f18c91db72ef
Frooty <77802339+FrootLoopJunkie@users.noreply.github.com>
2023-12-29 22:58:02 -0800

    updated description for link

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ function About() {
     return (
       <div className="AboutContainer">
         <h3 className="AboutHeader">About Us</h3>
-          <span className="AboutContent">Nestled in the heart of Everett, Washington, Salish Sparkle Home Cleaning is more than just a cleaning service; we're a part of your community. Our journey began with a simple mission: 
+          <span className="AboutContent">Nestled in the heart of Everett, Washington, Salish Sparkle Cleaning is more than just a cleaning service; we're a part of your community. Our journey began with a simple mission: 
             to bring a new level of shine and satisfaction to every home we touch. We understand the importance of a clean, comfortable living space, and we dedicate ourselves to ensuring yours sparkles like never before.</span>
       </div>
     );
