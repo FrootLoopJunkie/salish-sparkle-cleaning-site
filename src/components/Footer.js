40db0d7b07d1f7d4c144097fc704109f51d0088a
Frooty <77802339+FrootLoopJunkie@users.noreply.github.com>
2023-12-29 23:32:59 -0800

    phonenumber link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
       <div className="FooterContainer">
-          <p className="FooterContent">-| Salish Sparkle Cleaning | Established 2023 | Everett Local, Everett Strong | Contact: <a href="mailto: sales@salishsparklecleaning.com">sales@salishsparklecleaning.com</a> - (425) 521-7835 |-</p>
+          <p className="FooterContent">-| Salish Sparkle Cleaning | Established 2023 | Everett Local, Everett Strong | Contact: <a href="mailto: sales@salishsparklecleaning.com">sales@salishsparklecleaning.com</a> - <a href="tel:425-521-7835">(425) 521-7835</a> |-</p>
 
       </div>
     );
