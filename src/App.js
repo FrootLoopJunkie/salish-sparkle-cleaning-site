75a4a63b15928bab61abab633c105593be15376a
Frooty <77802339+FrootLoopJunkie@users.noreply.github.com>
2024-01-02 01:31:58 -0800

    new image

--- src/App.js
+++ src/App.js
@@ -24,17 +24,7 @@ function App() {
         />
         <ImageSlideshow
           images={[
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless",
-            "https://media.discordapp.net/attachments/222776929256734722/1187693780879544402/Salish_Sparkle_Cleaning_Icon.png?ex=65a10b87&is=658e9687&hm=562d4c1b66ed7738535402225a715e7041d06dc0b9e9b21c52964b4b56450756&=&format=webp&quality=lossless"
+            "https://media.discordapp.net/attachments/222776929256734722/1191671574902865930/Salish_Sparkle_Cleaning_Icon_variant2.png?ex=65a649a3&is=6593d4a3&hm=3077b265068cc39a6a8cbc27c55c2004732895a8c2df40497325964ee1de6045&=&format=webp&quality=lossless"
           ]}
           currentPage
           changePageTo={changePageTo}
@@ -69,14 +59,26 @@ function App() {
         />
         <div className='HomeCleaningPageContainer'>
           <h3 className='ServicesOffered'>Home Cleaning</h3>
-          <span>1 Bedroom Home Estimated $105 @ 3 hours <br /> 3 Bedroom Home Estimated $175 @ 5 hours <br />  Basic Service $35/h*</span>
+          <span>1 Bedroom Home Estimated $90 @ 3 hours <br /> 3 Bedroom Home Estimated $150 @ 5 hours <br />  Basic Service $30/h*</span>
           <ConsultationButtonInline
             changePageTo={changePageTo}
           />
           <span className='ServicesOffered'>Services Offered</span>
           <div className='FlexboxRow'>
-            <div className='FlexboxColumn ServiceContainerCateg'>
+          <div className='FlexboxColumn ServiceContainerCateg'>
               <span className='ServiceCatergory'>General</span>
+              <ul>
+                <li className='ServicesListItem'>Floor Care: <span className='ServicesListItemSubtext'>Sweeping and spot cleaning on all floor types</span></li>
+                <li className='ServicesListItem'>Mopping: <span className='ServicesListItemSubtext'>Hard floors</span></li>
+                <li className='ServicesListItem'>Glass Cleaning: <span className='ServicesListItemSubtext'>Mirrors, glass tables, and interior windows</span></li>
+                <li className='ServicesListItem'>Trash Removal: <span className='ServicesListItemSubtext'>Emptying bins and replacing liners</span></li>
+                <li className='ServicesListItem'>Surface Sanitization: <span className='ServicesListItemSubtext'>High-touch areas, doorknobs, light switches</span></li>
+                <li className='ServicesListItem'>Ceiling and Corner Web Removal: <span className='ServicesListItemSubtext'>Clearing webs and dust from ceilings and corners</span></li>
+                <li className='ServicesListItem'>Baseboard Cleaning: <span className='ServicesListItemSubtext'>Wiping down and dusting baseboards</span></li>
+              </ul>
+            </div>
+            <div className='FlexboxColumn ServiceContainerCateg'>
+              <span className='ServiceCatergory'>Kitchen</span>
               <ul>
                 <li className='ServicesListItem'>Appliance Cleaning: <span className='ServicesListItemSubtext'>Exterior of refrigerator, oven, microwave, dishwasher</span></li>
                 <li className='ServicesListItem'>Countertop Cleaning: <span className='ServicesListItemSubtext'>Including backsplashes, sanitizing surfaces</span></li>
@@ -100,19 +102,7 @@ function App() {
                 <li className='ServicesListItem'>Floor Sweeping and Mopping: <span className='ServicesListItemSubtext'>Special attention to corners and behind the toilet</span></li>
                 <li className='ServicesListItem'>Cabinet and Drawer Exterior: <span className='ServicesListItemSubtext'>Wiping down surfaces</span></li>
                 <li className='ServicesListItem'>Exhaust Fan Dusting: <span className='ServicesListItemSubtext'>Removing dust and buildup</span></li>
-                <li className='ServicesListItem'>Towel Replacement: <span className='ServicesListItemSubtext'>Changing towels if provided</span></li>
-              </ul>
-            </div>
-            <div className='FlexboxColumn ServiceContainerCateg'>
-              <span className='ServiceCatergory'>General</span>
-              <ul>
-                <li className='ServicesListItem'>Floor Care: <span className='ServicesListItemSubtext'>Sweeping and spot cleaning on all floor types</span></li>
-                <li className='ServicesListItem'>Mopping: <span className='ServicesListItemSubtext'>Hard floors</span></li>
-                <li className='ServicesListItem'>Glass Cleaning: <span className='ServicesListItemSubtext'>Mirrors, glass tables, and interior windows</span></li>
-                <li className='ServicesListItem'>Trash Removal: <span className='ServicesListItemSubtext'>Emptying bins and replacing liners</span></li>
-                <li className='ServicesListItem'>Surface Sanitization: <span className='ServicesListItemSubtext'>High-touch areas, doorknobs, light switches</span></li>
-                <li className='ServicesListItem'>Ceiling and Corner Web Removal: <span className='ServicesListItemSubtext'>Clearing webs and dust from ceilings and corners</span></li>
-                <li className='ServicesListItem'>Baseboard Cleaning: <span className='ServicesListItemSubtext'>Wiping down and dusting baseboards</span></li>
+                <li className='ServicesListItem'>**Towel Replacement: <span className='ServicesListItemSubtext'>Changing towels if provided</span></li>
               </ul>
             </div>
           </div>
